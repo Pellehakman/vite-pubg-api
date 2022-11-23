4295bd9da51fa41ca55d6b0fd78f971ba886c104
Pellehakman <perhakman@outlook.com>
2022-11-23 23:28:23 +0100

    Update config.tsx

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -1,14 +1,14 @@
 // Import the functions you need from the SDKs you need
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyDaVNlnhJ13G3i5koYdlXx_Za8rB8AIRsk",
-    authDomain: "pubg-app-7d22e.firebaseapp.com",
-    projectId: "pubg-app-7d22e",
-    storageBucket: "pubg-app-7d22e.appspot.com",
-    messagingSenderId: "114350787026",
-    appId: "1:114350787026:web:7a551a0d2388cdfc23e00b",
-    measurementId: "G-MCRB3LLBCT"
-  };
+  apiKey: "AIzaSyA8dXNBluFv612z-eJjn7ierzaaONn8ZQk",
+  authDomain: "pubg-vite-app.firebaseapp.com",
+  projectId: "pubg-vite-app",
+  storageBucket: "pubg-vite-app.appspot.com",
+  messagingSenderId: "293051420874",
+  appId: "1:293051420874:web:9836823a50f86f99882433",
+  measurementId: "G-BK8J3CS81V"
+};
   
 
 
